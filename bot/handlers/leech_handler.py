c15b36c693648634cb6ed2021efb87bb1b969543
Aza Maulana <azamaulanaaa@gmail.com>
2020-08-16 03:35:30 +0700

    remove main message no matter what

--- bot/handlers/leech_handler.py
+++ bot/handlers/leech_handler.py
@@ -48,7 +48,6 @@ async def func(client : Client, message: Message):
                 os_path_join(dir, file.path),
                 reply
             )
-        download.remove(force=True, files=True)
     else:
         gids = download.followed_by_ids
         for gid in gids:
@@ -60,6 +59,7 @@ async def func(client : Client, message: Message):
                     reply
                 )
             aria2_api.get_download(gid).remove(force=True, files=True)
+    download.remove(force=True, files=True)
     await reply.delete()
     
 
