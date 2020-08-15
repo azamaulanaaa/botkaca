6313baa6e94dbcb63c0183948b45d3a8f453eefb
Aza Maulana <azamaulanaaa@gmail.com>
2020-08-15 18:31:55 +0700

    missing dash

--- bot/plugins/aria2.py
+++ bot/plugins/aria2.py
@@ -35,7 +35,7 @@ class aria2(aria2p.API):
                 "--enable-rpc"
             ]
             for key in self.__config:
-                cmd.append(f"{key}={self.__config[key]}")
+                cmd.append(f"--{key}={self.__config[key]}")
             LOGGER.info(cmd)
             self.__process = await asyncio.create_subprocess_exec(
                 *cmd,
