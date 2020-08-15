838341cb3bfaa6ba015dedd572a709fed355e6cd
Aza Maulana <azamaulanaaa@gmail.com>
2020-08-16 00:54:50 +0700

    update message so more readable

--- bot/locals/default.py
+++ bot/locals/default.py
@@ -6,7 +6,7 @@ LOCAL = Local({
     'WRONG_ROOM' : 'I a\'m not suppose to be here.\nID : <code>{CHAT_ID}</code>',
     'WELCOME_MESSAGE' : "Hi!\nI'm <b>Bot</b>!\nPowered by aiogram.\n\nUse <code>/{cmd_pass} </code>to enter the password.",
     'ARIA2_CHECKING_LINK' : "checking...",
-    'ARIA2_DOWNLOAD_STATUS' : "Name : {name}\nDN {download_speed} / UP {upload_speed}\nProgress : {progress_string}\nETA : {eta}\nID : <code>{gid}</code>",
+    'ARIA2_DOWNLOAD_STATUS' : "Name : <code>{name}</code>\nDN {download_speed} / UP {upload_speed}\nProgress : {progress_string}\nETA : {eta}\nID : <code>{gid}</code>",
     'ARIA2_DOWNLOAD_SUCCESS' : 'File downloaded : <code>{name}</code>',
     'ARIA2_DOWNLOAD_CANCELED' : 'Download canceled : <code>{name}</code>',
     'ARIA2_DEAD_LINK' : 'Download auto canceled : <code>{name}</code>\nYour Torrent/Link is Dead.',
