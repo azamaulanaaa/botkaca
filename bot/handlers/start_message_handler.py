0c21d58fdd8f679871aca50ccb6334bc76ad4513
Aza Maulana <azamaulanaaa@gmail.com>
2020-08-23 22:03:38 +0700

    adding github link

--- bot/handlers/start_message_handler.py
+++ bot/handlers/start_message_handler.py
@@ -7,6 +7,9 @@ async def func(client : Client, message: Message):
         await message.delete()
     except:
         pass
-    await message.reply_text(LOCAL.WELCOME_MESSAGE.format(cmd_pass = COMMAND.PASSWORD))
+    await message.reply_text(
+        LOCAL.WELCOME_MESSAGE.format(cmd_pass = COMMAND.PASSWORD),
+        disable_web_page_preview=True
+    )
     if not CONFIG.BOT_PASSWORD:
         await help_message_handler.func(client, message)
--- bot/locals/default.py
+++ bot/locals/default.py
@@ -5,7 +5,7 @@ LOCAL = Local({
     'HELP_MESSAGE_HEADER' : '<b>Bot Command</b>',
     'NO_HELP_INFO' : 'no information',
     'WRONG_ROOM' : 'I a\'m not suppose to be here.\nID : <code>{CHAT_ID}</code>',
-    'WELCOME_MESSAGE' : "Hi!\nI'm <b>Bot</b>!\nPowered by pyrogram.\n\nUse <code>/{cmd_pass} </code>to enter the password.",
+    'WELCOME_MESSAGE' : "Hi!\nI'm <b>Bot</b>!\nPowered by pyrogram.\nFork: <a href='https://github.com/azamaulanaaa/botkaca'>github</a>\n\nUse <code>/{cmd_pass} </code>to enter the password.",
     'LEECH_LIST_MESSAGE_HEADER' : '<b>Leech Status</b>',
     'LEECH_LIST_FORMAT' : 'Name: <code>{name}</code>\nStatus: {status}\nID: <code>{gid}</code>\n\n',
     'ARIA2_CHECKING_LINK' : "checking...",
